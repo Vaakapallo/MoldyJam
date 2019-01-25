397f90fda644a2b438ae762de1f2fbc0926be801
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2019-01-25 22:43:56 +0200

    the mold now spreads

---
+++ Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    private List<Tile> tiles;
+
+    public static GameManager instance;
+
+    void Awake(){
+        instance = this;
+        tiles = FindObjectsOfType<Tile>().ToList();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public List<Tile> GetNeighbours(int x, int y){
+        List<Tile> neighbours = new List<Tile>();
+        foreach(Tile t in tiles){
+            if(t.x == x + 1 || t.x == x - 1){
+                if(t.y == y){
+                    neighbours.Add(t);
+                }    
+            }
+            if(t.y == y + 1 || t.y == y - 1){
+                if(t.x == x){
+                    neighbours.Add(t);
+                }    
+            }
+        }
+        return neighbours;
+    }
+
+
+    public List<Tile> GetNeighboursBroken(int x, int y){
+        return tiles.Where(tile => Mathf.Abs(tile.x - x) <= 1 
+        && Mathf.Abs(tile.y - y) <= 1 
+        && !(x == tile.x && y == tile.y) 
+        && Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y) < 2).ToList();
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public float moldiness = 0f;
+    public bool moldy = true;
+    private string type = "Floor";
+    public List<Tile> neighbours;
+
+    private bool infected = false; 
+
+    public int x,y;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        x = Mathf.RoundToInt(transform.localPosition.x/0.2f);
+        y = Mathf.RoundToInt(transform.localPosition.y/0.2f);
+
+    }
+
+    void Start(){
+        neighbours = GameManager.instance.GetNeighbours(x, y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moldy){
+            moldiness += 0.05f * Random.Range(0f,1f);
+        }
+        if(moldiness >= 1 && !infected){
+            InfectNeighbours();
+        }
+        Color oldColor = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = new Color(0, moldiness, 0);
+    }
+
+    void InfectNeighbours(){
+        foreach(Tile t in neighbours){
+            print("infecting");
+            t.moldy = true;
+        }
+        infected = true;
+    }
+}
