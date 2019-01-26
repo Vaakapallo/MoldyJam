8840de863b058edcff580f5a9d56dd5570d13e05
Jussi Toivonen <jussi.toivonen@futureplay.fi>
2019-01-26 16:13:41 +0200

    Add a timescale slider to UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public MoldType chosenType = MoldType.Green;
+    public Slider timeScaleSlider;
 
     private List<Tile> tiles;
 
@@ -17,22 +19,18 @@ public class GameManager : MonoBehaviour
     void Awake(){
         instance = this;
         tiles = FindObjectsOfType<Tile>().ToList();
+        timeScaleSlider.onValueChanged.AddListener(val => ChangeTimescale(val));
     }
-
-    void FixedUpdate()
-    {
-        Time.timeScale = timeScale;
-    }
-
-    public List<Tile> GetNeighbours(int x, int y){
+    
+    public List<Tile> GetNeighbours(int x, int y) {
         List<Tile> neighbours = new List<Tile>();
         foreach(Tile t in tiles){
-            if(t.X == x + 1 || t.X == x - 1){
+            if(t.X == x + 1 || t.X == x - 1) {
                 if(t.Y == y){
                     neighbours.Add(t);
                 }    
             }
-            if(t.Y == y + 1 || t.Y == y - 1){
+            if(t.Y == y + 1 || t.Y == y - 1) {
                 if(t.X == x) {
                     neighbours.Add(t);
                 }    
@@ -54,6 +52,9 @@ public class GameManager : MonoBehaviour
         chosenType = MoldType.Yellow;
     }
 
+    private void ChangeTimescale(float timeScale) {
+        Time.timeScale = timeScale;
+    }
 
     public List<Tile> GetNeighboursBroken(int x, int y){
         return tiles.Where(tile => Mathf.Abs(tile.X - x) <= 1 
