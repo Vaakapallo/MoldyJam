b2a637ed2a4bdec587df567fca58fded9b688374
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2019-01-26 16:08:47 +0200

    Made locks care about mold color

--- Assets/Scripts/Unlock.cs
+++ Assets/Scripts/Unlock.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(Tile))]
 public class Unlock : MonoBehaviour
 {
+    private Tile tile;
     public Lock tileToUnlock;
+    public MoldType mold;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Tile>().OnSpread += UnlockTile;    
+        tile = GetComponent<Tile>();
+        tile.OnSpread += UnlockTile;    
     }
 
     void UnlockTile(){
-        tileToUnlock.Unlock();
+        if(mold == tile.moldType)
+            tileToUnlock.Unlock();
     }
 }
