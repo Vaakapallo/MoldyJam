d554e1b743ccfd0bc41c94d8ea730e95f6396ddd
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2019-01-27 14:31:55 +0200

    Doors are now automatically the right color

--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -8,15 +8,32 @@ public class Lock : MonoBehaviour
     private Tile tile;
     private List<Unlock> unlocks;
     private GameObject DoorSprite;
+    private OpenDoor open;
 
     void Awake(){
         tile = GetComponent<Tile>();
         unlocks = new List<Unlock>();
+        DoorSprite = Resources.Load<GameObject>("DoorFrame");
     }
 
     void Start()
     {
+        open = Instantiate(DoorSprite,this.transform).GetComponent<OpenDoor>();
         tile.moldProof = true;
+        Invoke("GetColorsAndSendToDoor", 0.03f);
+    }
+
+    private void GetColorsAndSendToDoor(){
+        List<MoldType> molds = new List<MoldType>();
+        foreach(Unlock u in unlocks){
+            if(!molds.Contains(u.mold))
+                molds.Add(u.mold);
+        }
+        if(molds.Count > 1){
+            open.DecideColors(molds[0], molds[1]);
+        } else {
+            open.DecideColors(molds[0], MoldType.None);
+        }
     }
 
     public void AddUnlocker(Unlock unlocker){
@@ -26,7 +43,7 @@ public class Lock : MonoBehaviour
     public void Unlock(){
         if(AllUnlocksDone()){
             tile.moldProof = false;
-            
+            open.Open();
         }
     }
 
--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class OpenDoor : MonoBehaviour
 {
-    
     public Sprite openedDoor;
     private SpriteRenderer sprite;
+    public Sprite cyanDoor;
+    public Sprite pinkDoor;
+    public Sprite purpleDoor;
+    public Sprite yellowDoor;
+    public Sprite yellowCyanDoor;
 
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
     }
 
+    public void DecideColors(MoldType mold1, MoldType mold2){
+        if(mold2 == MoldType.None){
+            if(mold1 == MoldType.Cyan){
+                sprite.sprite = cyanDoor;
+            } else if(mold1 == MoldType.Pink){
+                sprite.sprite = pinkDoor;
+            } else if(mold1 == MoldType.Purple){
+                sprite.sprite = purpleDoor;
+            } else if(mold1 == MoldType.Yellow){
+                sprite.sprite = yellowDoor;
+            }
+        }
+        else if(mold2 == MoldType.Cyan || mold2 == MoldType.Yellow
+        && mold1 == MoldType.Cyan || mold1 == MoldType.Yellow){
+            sprite.sprite = yellowCyanDoor;
+        }
+    }
+
     public void Open(){
-        sprite.sprite = openedDoor;
+        sprite.enabled = false;
     }
 }
