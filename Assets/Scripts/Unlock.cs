c0e17cb826ced8b2e2ec48caa79991b40ee10bd9
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2019-01-27 14:53:14 +0200

    added automatically adding unlock object sprites

--- Assets/Scripts/Unlock.cs
+++ Assets/Scripts/Unlock.cs
@@ -9,13 +9,22 @@ public class Unlock : MonoBehaviour
     public Lock tileToUnlock;
     public MoldType mold;
     private bool correct = false;
+    private GameObject UnlockSprite;
+    private UnlockerSprite unlocker;
+
+    void Awake()
+    {
+        UnlockSprite = Resources.Load<GameObject>("UnlockerObject");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        unlocker = Instantiate(UnlockSprite,this.transform).GetComponent<UnlockerSprite>();
         tile = GetComponent<Tile>();
         tile.OnSpread += UnlockTile;
         tileToUnlock.AddUnlocker(this);
+        unlocker.DecideColor(mold);
     }
 
     void UnlockTile(){
---
+++ Assets/UnlockerSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockerSprite : MonoBehaviour
+{
+    public Sprite PurpleApple;
+    public Sprite CyanFridge;
+    public Sprite YellowTrashBin;
+
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void DecideColor(MoldType mold){
+        if(mold == MoldType.Purple){
+            sprite.sprite = PurpleApple;
+        } else if(mold == MoldType.Cyan){
+            sprite.sprite = CyanFridge;
+        } else if(mold == MoldType.Yellow){
+            sprite.sprite = YellowTrashBin;
+        }
+    }
+}
